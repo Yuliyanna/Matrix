e1309e0cbde707267871d837c82626a761510bb7
Yuliyanna <bobrova.yuliya18@gmail.com>
2024-11-10 19:42:48 +0500

    +++

--- app/src/main/java/com/example/eisenhowermatrix/NotesList.kt
+++ app/src/main/java/com/example/eisenhowermatrix/NotesList.kt
@@ -1,8 +1,6 @@
 package com.example.eisenhowermatrix
 
 
-import android.widget.ImageButton
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
@@ -31,7 +29,6 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Icon
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.ui.res.painterResource
 
